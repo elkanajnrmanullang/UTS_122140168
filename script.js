c4933c596045554175d0e9a51183e18789228aef
elkanajnrmanullang <elkanamanullang7@gmail.com>
2024-10-20 01:41:15 +0700

    tinggal about, dan 2 page

---
+++ script.js
@@ -0,0 +1,5 @@
+// SIDEBAR
+const toggler = document.querySelector(".btn");
+toggler.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("collapsed");
+});
